2953871c1cd1a29c906b58ee0bd79e2510760ce7
Madhanmyana <madhanmyana99@gmail.com>
2026-07-16 23:06:26 +0530

    add authentication to create product and function to retreive all products

--- app/api/product.py
+++ app/api/product.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import Session
 from services.product_service import product_create
 from db.database import get_db
 from schemas.product import ProductCreate,ProductResponse,ProductUpdate
+from dependencies.dependencies import get_current_user
 
 
 router=APIRouter()
 
 @router.post('/createproduct',response_model=ProductResponse)
-def create_product(product:ProductCreate,db:Session=Depends(get_db)):
+def create_product(product:ProductCreate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
       return product_create(product,db)
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,11 +1,12 @@
-from fastapi import HTTPException
+from fastapi import HTTPException,Depends
 from sqlalchemy.orm import Session
 
 from models.product import Product
 from schemas.product import ProductCreate, ProductResponse, ProductUpdate
+from dependencies.dependencies import get_current_user
 
 def product_create(product:ProductCreate,db:Session):
-    if not db.query(Product).filter(Product.name==product.name):
+    if db.query(Product).filter(Product.name==product.name).first():
         raise HTTPException(status_code=400,detail='product alredy exist')
     
     new_product=Product(
@@ -19,3 +20,7 @@ def product_create(product:ProductCreate,db:Session):
     db.commit()
     db.refresh(new_product)
     return new_product
+
+def all_products(db:Session):
+    products=db.query(Products).all()
+    return products
