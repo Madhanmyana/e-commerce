720a9016e588b69f7a8d3d800385ead74bdb1cfb
Madhanmyana <madhanmyana99@gmail.com>
2026-07-17 21:36:55 +0530

    added get all products,get product by id,update product by id

--- app/api/product.py
+++ app/api/product.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
 
-from services.product_service import product_create
+from services.product_service import product_create,all_products,get_product_by_id,update_product_by_id
 from db.database import get_db
 from schemas.product import ProductCreate,ProductResponse,ProductUpdate
 from dependencies.dependencies import get_current_user
@@ -12,3 +12,15 @@ router=APIRouter()
 @router.post('/createproduct',response_model=ProductResponse)
 def create_product(product:ProductCreate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
       return product_create(product,db)
+
+@router.get('/get_all_product',response_model=list[ProductResponse])
+def get_all_product(db:Session=Depends(get_db)):
+      return all_products(db)
+
+@router.get('/get_product_by_id{id}',response_model=ProductResponse)
+def get_by_id(id:int,db:Session=Depends(get_db)):
+      return get_product_by_id(id,db)
+
+@router.put('/update_product_by_id',response_model=ProductResponse)
+def update_by_id(id:int,update_product:ProductUpdate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+      return update_product_by_id(id,update_product,db)
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -22,5 +22,30 @@ def product_create(product:ProductCreate,db:Session):
     return new_product
 
 def all_products(db:Session):
-    products=db.query(Products).all()
+    products=db.query(Product).all()
     return products
+
+def get_product_by_id(id:int,db:Session):
+    product=db.query(Product).filter(Product.id==id).first()
+    if product:
+        return product
+
+def update_product_by_id(id:int,update_product:ProductUpdate,db:Session):
+    product=db.query(Product).filter(Product.id==id).first()
+    if product:
+        product.name=update_product.name
+        product.description=update_product.description
+        product.price=update_product.price
+        product.stock=update_product.stock
+        return product
+    raise HTTPException(status_code=404,detail='Product not exist')
+
+def delete_product_by_id(id:int):
+    product=db.query(Product).filter(product.id==id).first()
+    if product:
+        db.delete(product)
+        db.commit()
+        db.refresh(product)
+        return product
+
+    raise HTTPException(status_code=404,detail='Product not exist')
