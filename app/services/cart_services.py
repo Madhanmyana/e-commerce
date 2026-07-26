766f2e953c8b9b632c2a175f09b4b452b7171ac1
Madhanmyana <madhanmyana99@gmail.com>
2026-07-26 11:25:02 +0530

    added update_cart_item

--- app/services/cart_services.py
+++ app/services/cart_services.py
@@ -1,6 +1,6 @@
 from fastapi import Session,HTTPException
 
-from schemas.cart import AddToCartRequest
+from schemas.cart import AddToCartRequest,UpdateCartItemRequest
 from models.product import Product
 from models.cart import Cart,CartItem
 
@@ -47,5 +47,23 @@ def add_to_cart(user_id: int, cart: AddToCartRequest, db: Session):
                 "items": user_cart.items
                 }
 
+        raise HTTPException(status_code=404,detail='product not found')
+    raise HTTPException(status_code=400,detail='bad request')
+
+def update_cart_item(user_id:int, cart:UpdateCartItemRequest, db:Session):
+    if cart.quantity>0:
+        if db.query(Product).filter(Product.id==cart.cart_item_id).first():
+            user_cart=db.query(Cart).filter(Cart.user_id == user_id).first()
+            if user_cart:
+                existing_cart_item=db.query(CartItem).filter(CartItem.cart_id==user_cart.id,CartItem.product_id==cart.product_id).first()
+                if existing_cart_item:
+                    existing_cart_item.quantity=cart.quantity
+                    db.commit()
+                    db.refresh(existing_cart_item)
+                    return {
+                        'items':user_cart.items
+                        }
+                raise HTTPException(status_code=404,detail='item not found')
+            raise HTTPException(status_code=404,detail='cart not found')
         raise HTTPException(status_code=404,detail='product not found')
     raise HTTPException(status_code=400,detail='bad request')
